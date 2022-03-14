4c242304c03bfa15960d459bc08018887e75e686
Kai Shen <shenkai029@gmail.com>
2022-03-13 21:54:17 -0700

    * Create Log class and feature based on spglog

--- Hazel_Kai/src/Hazel/EntryPoint.h
+++ Hazel_Kai/src/Hazel/EntryPoint.h
@@ -4,7 +4,12 @@
 
 extern Hazel::Application* Hazel::CreateApplication();
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 4;
+	HZ_INFO("Hello! a = {0}", a);
 
 	std::cout << "Hazel Engine running..." << std::endl;
 	auto app = Hazel::CreateApplication();
