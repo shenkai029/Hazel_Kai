a446e8dd79fbc52efc823eaca4ca442895031d80
Kai Shen <shenkai029@gmail.com>
2022-03-14 22:01:39 -0700

    * Re-generate the overall solution using premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Hazel_Kai"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel_Kai"
+	location "Hazel_Kai"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "on"
+		systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox" )
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		optimize "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs 
+	{
+		"Hazel_Kai/vendor/spdlog/include",
+		"Hazel_Kai/src"
+	}
+
+	links
+	{
+		"Hazel_Kai"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "on"
+		systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		optimize "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
