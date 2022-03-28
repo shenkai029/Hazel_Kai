858dfeccf46caa5fdef58a4516bfc09d8d6a7948
Kai Shen <shenkai029@gmail.com>
2022-03-27 20:55:04 -0700

    * Create premake file for GLAD and re-gen the project

--- Hazel_Kai/vendor/GLAD/premake5.lua
+++ Hazel_Kai/vendor/GLAD/premake5.lua
@@ -1,129 +1,34 @@
-workspace "Hazel_Kai"
-	architecture "x64"
-
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
--- Inlcude directories relative to root folder(solution dir)
-IncludeDir = {}
-IncludeDir["GLFW"] = "Hazel_Kai/vendor/GLFW/include"
-
-include "Hazel_Kai/vendor/GLFW"
-
-project "Hazel_Kai"
-	location "Hazel_Kai"
-	kind "SharedLib"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "hzpch.h"
-	pchsource "Hazel_Kai/src/hzpch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs 
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}"
-	}
-
-	links
-	{
-		"GLFW",
-		"opengl32.lib"
-	}
-
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "on"
-		systemversion "latest"
-
-		defines
-		{
-			"HZ_PLATFORM_WINDOWS",
-			"HZ_BUILD_DLL"
-		}
-
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox" )
-		}
-
-	filter "configurations:Debug"
-		defines "HZ_DEBUG"
-		buildoptions "/MDd"
-		optimize "On"
-
-	filter "configurations:Release"
-		defines "HZ_RELEASE"
-		buildoptions "/MD"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "HZ_DIST"
-		buildoptions "/MD"
-		optimize "On"
-
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
+project "GLAD"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"include/glad/glad.h",
+		"include/KHR/khrplatform.h",
+		"src/glad.c"
 	}
 
-	includedirs 
+	includedirs
 	{
-		"Hazel_Kai/src",
-		"Hazel_Kai/vendor/spdlog/include"
-	}
-
-	links
-	{
-		"Hazel_Kai"
+		"include"
 	}
 
 	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "on"
+		buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
-
-		defines
-		{
-			"HZ_PLATFORM_WINDOWS",
-		}
+		staticruntime "On"
 
 	filter "configurations:Debug"
-		defines "HZ_DEBUG"
+		runtime "Debug"
 		buildoptions "/MDd"
 		optimize "On"
 
 	filter "configurations:Release"
-		defines "HZ_RELEASE"
+		runtime "Release"
 		buildoptions "/MD"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "HZ_DIST"
-		buildoptions "/MD"
-		optimize "On"
+		optimize "on"
